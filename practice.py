d34d7a31718e03515ddf69df846ffd85d216cdfa
Lee JinKyu <jaydeen@kakao.com>
2022-06-11 00:08:13 +0900

    파이썬 첫날(2022.06.11)

---
+++ practice.py
@@ -0,0 +1,43 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*9)
+print(3*(3+1))
+
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
+
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5>10))
+
+# 애완동물을 소개해 주세요~
+animal = " 강아지"
+name = "새롬이"
+age = 14
+hobby = "먹는걸"
+is_grandmom = age >= 10
+
+print("우리집 "+animal+"의 이름은 "+name+"예요")
+hobby = "박경희를"
+# ,(콤마)로 문자열을 합쳤을 경우 띄어쓰기가 하나 들어감
+print("새롬이는 ",age,"살이며, ",hobby," 아주 좋아해요")
+print("새롬이는 할머니일까요? "+str(is_grandmom))
+
+'''
+여러줄 주석
+1
+2
+3
+4
+5
+
+'''
---
+++ random_function.py
@@ -0,0 +1,27 @@
+from random import *
+
+# print(random()) # 0.0 ~ 1.0 임의의 값
+# print(random()*10) # 0.0 ~ 10.0
+# print(int(random()*10)) # 0 ~ 10 사이의 정수
+# print(int(random()*10)+1) # 1 ~ 10 사이의 정수
+
+# print(int(random() * 45)+1) # 1~45
+# print(int(random() * 45)+1) # 1~45
+# print(int(random() * 45)+1) # 1~45
+# print(int(random() * 45)+1) # 1~45
+# print(int(random() * 45)+1) # 1~45
+# print(int(random() * 45)+1) # 1~45
+
+print(randrange(1,46)) # 1~45
+print(randrange(1,46)) # 1~45
+print(randrange(1,46)) # 1~45
+print(randrange(1,46)) # 1~45
+print(randrange(1,46)) # 1~45
+print(randrange(1,46)) # 1~45
+
+print(randint(1,45)) # 1~45
+print(randint(1,45)) # 1~45
+print(randint(1,45)) # 1~45
+print(randint(1,45)) # 1~45
+print(randint(1,45)) # 1~45
+print(randint(1,45)) # 1~45
